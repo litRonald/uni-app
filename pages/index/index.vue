6fdd4349b8a05df8d7ee88eca120d19146ab03ed
Your Nameqwyong <875682652@qq.com>
2022-05-19 22:17:32 +0800

    uni-app

---
+++ pages/index/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="container">
+		
+		<view>
+			<text @click="say">tewte</text>
+			<view>{{msg}}</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui',
+				msg: ''
+			}
+		},
+		methods: {
+			say() {
+				this.msg = 'HELLO'
+			}
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
